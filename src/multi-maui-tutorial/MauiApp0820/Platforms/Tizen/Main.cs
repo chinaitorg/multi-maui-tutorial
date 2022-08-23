7c316549a13f23de1d49d4304cb74b889f83ed34
JeremyWu917 <1280442096@qq.com>
2022-08-23 20:21:00 +0800

    complete first official MAUI demo

---
+++ src/multi-maui-tutorial/MauiApp0820/Platforms/Tizen/Main.cs
@@ -0,0 +1,17 @@
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using System;
+
+namespace MauiApp0820
+{
+    internal class Program : MauiApplication
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            app.Run(args);
+        }
+    }
+}
